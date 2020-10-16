e54ed94a910d4d88c0fe191388ce222d837c3392
Akash Gautam <info@appstone.in>
2020-10-16 18:47:31 +0530

    layout created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Avatar,
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemAvatar,
+  Typography
+} from "@material-ui/core";
+import React from "react";
+import ns from "./ns.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://nextstacks-backend-01.herokuapp.com/v1/get_active_domains", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+
+      }),
+    }).then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson)
+        if (responseJson.status) {
+          var result = [];
+          responseJson.result.map((s) => (
+            result.push(s)
+          ))
+
+          this.setState({
+            data: result
+          })
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    console.log(this.state.data);
+    return (
+      <Grid container>
+        <Grid item xs={12}>
+          <img 
+            src={ns}
+            style={{width: 100}} 
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Typography
+            variant="h5"
+          >
+            NEXTSTACKS DOMAINS
+          </Typography>
+        </Grid>
+        <List>
+          {this.state.data.map((s, index) => (
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar src={s.domain_image} />
+              </ListItemAvatar>
+              <ListItemText
+                primary={s.domain_name}
+                secondary={s.short_description}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </Grid>
+    )
+  }
 }
-
-export default App;
